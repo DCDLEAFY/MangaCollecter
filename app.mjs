cda5268b2bc1ceea6213040ec2e8d3ff5d24d96e
dcdleafy <allgaier.alif@gmail.com>
2022-03-23 16:32:49 +0000

    Update: app.mjs to hold query functions

--- app.mjs
+++ app.mjs
@@ -1,12 +1,14 @@
 import pkg from '@specify_/mangascraper';
 const { Manganato } = pkg;
 
+
+
 var savedMangaName = [];
 var savedMangaUrl = []
 
 //Save Manga with Manganato URL
 async function saveMangaUrl(mangaUrl){
-    const manganato = new Manganato();
+    
 
     const meta = await manganato.getMangaMeta(mangaUrl);
     
@@ -18,7 +20,6 @@ async function saveMangaUrl(mangaUrl){
 //Save Manga with Manganato Name
 async function saveMangaName(mangaName){
 
-    const manganato = new Manganato();
 
     const mangas = await manganato.search(mangaName);
     const meta = await manganato.getMangaMeta(mangas[0].url);
@@ -28,14 +29,66 @@ async function saveMangaName(mangaName){
     savedMangaUrl.push(meta.title);
 }
 
-async function latestChapterWithName(searchItem){
-
+//Query Manga for information
+async function queryMangaName(mangaName){
     const manganato = new Manganato();
 
-    const mangas = await manganato.search(searchItem);
+    const mangas = await manganato.search(mangaName);
     const meta = await manganato.getMangaMeta(mangas[0].url);
-    console.log(meta.title + "\n" + meta.chapters[0]);
-
-    // const images = await manganato.getPages(meta.chapters[0].url);
-    // console.log(images);
+    console.log(meta.title.main + " -> Chapters: " + meta.chapters.length);
 }
+
+//Get Pages links of URL
+async function getPageLinks(url){
+
+    const manganato = new Manganato();
+    const meta = await manganato.getMangaMeta(url);
+    const pageLinks = await manganato.getPages(meta.chapters[0].url);
+    console.log(images);
+}
+
+await getPageLinks('https://readmanganato.com/manga-gf983740');
+
+// class Factory{
+//     constructor(choice, stringy){
+//         this. manganato = new Manganato();
+//         this.choice = choice;
+//         this.stringy = stringy;
+//     }
+
+//     async ifUrl(){
+//         try{
+//             this.meta = await this.manganato.getMangaMeta(this.stringy);
+//         } catch (e){
+//             console.log(e);
+//         }
+        
+//     }
+
+//     async ifName(){
+//         try {
+//             this.mangas = await this.manganato.search(this.stringy);
+//             this.meta = await this.manganato.getMangaMeta(this.mangas[0].url);
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     }
+
+//     async init(){
+//         if(this.choice=='name'){
+//             this.ifName();
+//         } else {
+//             this.ifUrl();
+//         }
+
+//         return this.meta;
+//     }
+
+//     get meta(){
+//         return this.meta;
+//     }
+
+//     set meta(meta){
+//         this.meta = meta;
+//     }
+// }
